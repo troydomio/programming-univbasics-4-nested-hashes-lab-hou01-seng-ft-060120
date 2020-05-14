4cbc8509e70178b000f9d1ac3bd62fdc572c98d9
Troy Domio <troyakins99@yahoo.com>
2020-05-14 01:03:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -82,7 +82,18 @@ def adding_matz
       }
     }
 
+newbie = {
+  :yukihiro_matsumoto =>{
+    :known_for => "Ruby",
+    :languages => ["LISP","C"]
+  }
+}
+
+programmer_hash.merge!(newbie)
+
+ programmer_hash
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
